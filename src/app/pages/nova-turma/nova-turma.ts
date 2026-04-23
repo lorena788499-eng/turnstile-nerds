c206b45d36f2181078f147cabb64af6090eb85d4
ANALICE MARTINS RIBEIRO <analice.ribeiro@portalsesisp.org.br>
2026-04-23 15:54:37 -0300

    Feita página nova-turma

--- src/app/pages/nova-turma/nova-turma.ts
+++ src/app/pages/nova-turma/nova-turma.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import { BotaoCancelar } from "../../shared/botao-cancelar/botao-cancelar";
-import { BotaoConfirmar } from "../../shared/botao-confirmar/botao-confirmar";
+import { FormsModule } from '@angular/forms';
 
+/* 👇 ADICIONA ISSO */
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-nova-turma',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, BotaoCancelar, BotaoConfirmar,],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
   templateUrl: './nova-turma.html',
-  styleUrl: './nova-turma.css',
+  styleUrls: ['./nova-turma.css']
 })
-export class NovaTurma {}
+export class NovaTurma {
+
+  turma = {
+    nome: '',
+    periodo: '', // continua normal
+    ano: '',
+    capacidade: 0,
+    professor: ''
+  };
+
+  salvar() {
+    console.log('Dados da turma:', this.turma);
+    alert('Turma salva com sucesso!');
+  }
+}
